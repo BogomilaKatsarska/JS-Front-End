207cb4fb611fc0b919d29e721a54ee3aabfd3226
Bogomila Katsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2023-11-12 21:12:55 +0200

    Add files via upload

---
+++ 03.Exercise/02.add_and_subtract.js
@@ -0,0 +1,23 @@
+function addAndSubtract(first, second, third){
+    let sum = (first, second) => {
+        return first + second
+    }
+
+    let result = sum(first, second) - third;
+
+    console.log(result);
+}
+
+function addAndSubtractSecond(first, second, last){
+    
+    let sum = (first, second) => {
+        return first + second;
+
+    };
+
+    let subtract = (sum, lastNumber) => {
+        return sum - lastNumber;
+    };
+
+    console.log(subtract(sum(first, second), last));
+}
---
+++ 03.Exercise/06.pass_validator.js
@@ -0,0 +1,38 @@
+function passValidator(password){
+    function isValidLength(text){
+        return text.length  >= 6 && text.length <= 10;
+    }
+    function isAlphanumeric(text){
+        // test returns booleam
+        let regex = /^[a-zA-Z0-9]+$/;
+        let isCorrect = regex.test(text);
+        
+        return isCorrect;
+    }
+    function checkDigits(text){
+        let digitsCounter = 0;
+        for(const digit of text){
+            if(!isNaN(digit)){
+                digitsCounter++;
+            }
+        }
+        return digitsCounter >= 2;
+    }
+
+    let validLength = isValidLength(password);
+    let validNumeric = isAlphanumeric(password);
+    let validDigitsCount = checkDigits(password);
+
+    if (!validLength){
+        console.log('Password must be between 6 and 10 characters');
+    }
+    if (!validNumeric){
+        console.log('Password must consist only of letters and digits');
+    }
+    if (!validDigitsCount){
+        console.log('Password must have at least 2 digits');
+    }
+    if (validLength && validNumeric && validDigitsCount) {
+        console.log('Password is valid');
+    }
+}
