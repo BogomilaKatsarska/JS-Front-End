24162f2b15fded6d9a401715f8601cb03ba7ee06
Bogomila Katsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2023-12-06 15:36:37 +0200

    Add files via upload

---
+++ 05.Exercise/04.Show more/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const hiddenTextEl = document.getElementById('text')
+    hiddenTextEl.style.display = 'inline';
+
+    const readMoreBtn = document.querySelector('#more');
+    readMoreBtn.style.display='none';
+}
